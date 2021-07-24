8b894d7e99d17e03049ab13cd06e4502d8ed56f8
deepakkumar <kumar.de@husky.neu.edu>
2021-07-24 15:25:58 -0400

    add signup page (#13)
    * add signup page
    
    * Update strings.xml

--- app/src/main/java/edu/neu/madcourse/crack_it_up/Signup.java
+++ app/src/main/java/edu/neu/madcourse/crack_it_up/Signup.java
@@ -2,7 +2,9 @@ package edu.neu.madcourse.crack_it_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup extends AppCompatActivity {
 
@@ -11,4 +13,27 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    // called upon clicking Login button
+    public void onClickSignUpButton(View view) {
+        System.out.println("Sign up button clicked");
+
+
+        Intent intent1 = new Intent(Signup.this, HomeScreenActivity.class);
+
+        startActivity(intent1);
+
+    }
+
+    public void onClicklogin(View view) {
+        System.out.println("login text clicked");
+
+
+        Intent intent1 = new Intent(Signup.this, LoginActivity.class);
+
+        startActivity(intent1);
+
+    }
+
+
 }
