cc071e5b6a65274797ba6a64823c10fb2a0fa23b
deepakkumar <kumar.de@husky.neu.edu>
2021-07-26 08:28:29 -0400

    added login logic (#26)

--- app/src/main/java/edu/neu/madcourse/crack_it_up/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/crack_it_up/LoginActivity.java
@@ -1,16 +1,21 @@
 package edu.neu.madcourse.crack_it_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -21,46 +26,74 @@ import com.google.firebase.iid.internal.FirebaseInstanceIdInternal;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class LoginActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
     FirebaseAuth fAuth;
-
+    TextInputLayout mEmail, mPassword;
+    Button mLoginButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        //get the views
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        mLoginButton = findViewById(R.id.login);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
         fAuth = FirebaseAuth.getInstance();
         //if user is already logged in, direct to main activity
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
             finish();
         }
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getEditText().getText().toString().trim();
+                String password = mPassword.getEditText().getText().toString();
+
+                //validations
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    mEmail.requestFocus();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    mPassword.requestFocus();
+                    return;
+                }
+
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull  Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, HomeScreenActivity.class));
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Login failed! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+
+                    }
+                });
+
+            }
+            });
     }
 
-    // called upon clicking Login button
-    public void onClickLoginButton(View view) {
-        System.out.println("Login button clicked");
 
-
-//        TextView usernameTextView = (TextView) findViewById(R.id.userName);
-//        String username = usernameTextView.getText().toString().replaceAll("\\s", "");
-
-        TextInputLayout textInputLayout = findViewById(R.id.userName);
-        String username = textInputLayout.getEditText().getText().toString().replaceAll("\\s", "");
-        ;
-
-
-        if (username.length() == 0) {
-            Toast myToast = Toast.makeText(this, "Enter Username!", Toast.LENGTH_LONG);
-            myToast.show();
-        }else {
-            Intent intent1 = new Intent(LoginActivity.this, HomeScreenActivity.class);
-            intent1.putExtra("USERNAME", username);
-            startActivity(intent1);
-        }
-
-    }
 
     // called upon clicking Login button
     public void onClickSignUp(View view) {
