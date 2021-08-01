6913c0246bdab6f87f4b48acecffb93df0f9d5e9
kmmusemb <musembi.k@northeastern.edu>
2021-08-01 15:44:52 -0400

    Added activity results functionality (#66)
    Co-authored-by: musembik <77795930+musembik@users.noreply.github.com>

--- app/src/main/java/edu/neu/madcourse/crack_it_up/QuizResults.java
+++ app/src/main/java/edu/neu/madcourse/crack_it_up/QuizResults.java
@@ -1,8 +1,12 @@
 package edu.neu.madcourse.crack_it_up;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class QuizResults extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class QuizResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+
+        final AppCompatButton startNewBtn = findViewById(R.id.startNewQuizBtn);
+        final TextView correctAnswer = findViewById(R.id.correctAnswers);
+        final TextView incorrectAnswers = findViewById(R.id.incorrectAnswers);
+
+        final int getCorrectAnswers = getIntent().getIntExtra("correct", 0);
+        final int getIncorrectAnswers = getIntent().getIntExtra("incorrect", 0);
+
+        correctAnswer.setText(String.valueOf(getCorrectAnswers));
+        incorrectAnswers.setText(String.valueOf(getIncorrectAnswers));
+
+        startNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(QuizResults.this, TopicSelection.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(QuizResults.this, TopicSelection.class));
+        finish();
     }
 }
